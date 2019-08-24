e44efce1c8707f096e93dc50831e955246f23dc5
Jonn Dillon <jonn_dillon@iinet.net.au>
2019-08-24 16:54:55 +1000

    Added 'AI'

--- server/src/controllers/routes.ts
+++ server/src/controllers/routes.ts
@@ -1,8 +1,15 @@
 import { Router, Request, Response } from "express";
 const routes = Router();
 
+import { AnwserGenerator } from "./../content/anwsers";
+
 routes.post("/", (req: Request, res: Response) => {
     res.json({ message: "API is active"});
+});
+
+routes.post("/question", (req: Request, res: Response) => {
+    const result = AnwserGenerator.generator();
+    res.json({ answer: result, quote: (result.length * 1000) });
 });
 
 export default routes;
