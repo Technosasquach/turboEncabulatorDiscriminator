98cd5508b289251aeff29ac3da1bfb7fbf92b275
Kieran Luxford <kieranluxford11@gmail.com>
2019-08-24 18:44:13 +1000

    Updated Word List

--- server/src/content/words.ts
+++ server/src/content/words.ts
@@ -13,7 +13,19 @@ export const WordsTechnoBabbleProNouns: string[] = [
     "Encabulator",
     "Conductor",
     "Drive",
-    "Wayne Shaft"
+    "Wayne Shaft",
+    "Manifold", 
+    "Resistor", 
+    "Amplifier", 
+    "Canister", 
+    "Capacitor", 
+    "Coil", 
+    "Circut", 
+    "Conduit", 
+    "Core", 
+    "Driver", 
+    "Injector", 
+    "Oscillator"
 ];
 
 export const WordsTechnoBabbleDescription: string[] = [
@@ -38,6 +50,23 @@ export const WordsTechnoBabbleDescription: string[] = [
     "Potted",
     "Electrolydic",
     "Thin film",
+    "Analog", 
+    "Asynchronous", 
+    "Real-time", 
+    "Nested", 
+    "RS-232", 
+    "Augmented", 
+    "Auxiliary", 
+    "Artificial", 
+    "Harmonic", 
+    "Insulated", 
+    "Magnetic", 
+    "Modulated", 
+    "Primary", 
+    "Secondary", 
+    "Backup", 
+    "Control", 
+    "Optical" 
 ];
 
 export const WordsCar: string[] = [
